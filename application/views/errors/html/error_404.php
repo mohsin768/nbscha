9a1c979122e13cd91cc343c0a63af1c6b8a8457d
Varun Victor <varun.victor@fuego.qa>
2022-03-16 20:29:37 +0530

    404 Fix v3

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -12,54 +12,54 @@
 <!-- Page Title -->
 <title>NBSCHA</title>
 
-
 <!-- Favicon and Touch Icons -->
-<link href="<?php echo frontend_assets_url('public/images/favicon/favicon-32x32.png'); ?>" rel="shortcut icon" type="image/png">
+<link href="public/frontend/images/favicon.png" rel="shortcut icon" type="image/png">
+<link href="public/frontend/images/apple-touch-icon.png" rel="apple-touch-icon">
+<link href="public/frontend/images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+<link href="public/frontend/images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+<link href="public/frontend/images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
 
 <!-- Stylesheet -->
-<link href="<?php echo frontend_assets_url('css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
-<link href="<?php echo frontend_assets_url('css/jquery-ui.min.css'); ?>" rel="stylesheet" type="text/css">
-<link href="<?php echo frontend_assets_url('css/animate.css'); ?>" rel="stylesheet" type="text/css">
-<link href="<?php echo frontend_assets_url('css/css-plugin-collections.css'); ?>" rel="stylesheet"/>
+<link href="public/frontend/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="public/frontend/css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+<link href="public/frontend/css/animate.css" rel="stylesheet" type="text/css">
+<link href="public/frontend/css/css-plugin-collections.css" rel="stylesheet"/>
 <!-- CSS | menuzord megamenu skins -->
-<link id="menuzord-menu-skins" href="<?php echo frontend_assets_url('css/menuzord-skins/menuzord-rounded-boxed.css'); ?>" rel="stylesheet"/>
+<link id="menuzord-menu-skins" href="public/frontend/css/menuzord-skins/menuzord-rounded-boxed.css" rel="stylesheet"/>
 <!-- CSS | Main style file -->
-<link href="<?php echo frontend_assets_url('css/style-main.css'); ?>" rel="stylesheet" type="text/css">
+<link href="public/frontend/css/style-main.css" rel="stylesheet" type="text/css">
 <!-- CSS | Preloader Styles -->
-<link href="<?php echo frontend_assets_url('css/preloader.css'); ?>" rel="stylesheet" type="text/css">
+<link href="public/frontend/css/preloader.css" rel="stylesheet" type="text/css">
 <!-- CSS | Custom Margin Padding Collection -->
-<link href="<?php echo frontend_assets_url('css/custom-bootstrap-margin-padding.css'); ?>" rel="stylesheet" type="text/css">
+<link href="public/frontend/css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
 <!-- CSS | Responsive media queries -->
-<link href="<?php echo frontend_assets_url('css/responsive.css'); ?>" rel="stylesheet" type="text/css">
+<link href="public/frontend/css/responsive.css" rel="stylesheet" type="text/css">
 <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
-<link href="<?php echo frontend_assets_url('css/style.css'); ?>" rel="stylesheet" type="text/css">
-
-<!-- Revolution Slider 5.x CSS settings -->
-<link  href="<?php echo frontend_assets_url('css/revolution-slider/css/settings.css'); ?>" rel="stylesheet" type="text/css"/>
-<link  href="<?php echo frontend_assets_url('css/revolution-slider/css/layers.css'); ?>" rel="stylesheet" type="text/css"/>
-<link  href="<?php echo frontend_assets_url('css/revolution-slider/css/navigation.css'); ?>" rel="stylesheet" type="text/css"/>
-<link  href="<?php echo frontend_assets_url('css/jquery.multiselect.css'); ?>" rel="stylesheet" type="text/css"/>
+<!-- <link href="public/frontend/css/style.css" rel="stylesheet" type="text/css"> -->
 
 <!-- CSS | Theme Color -->
-<link href="<?php echo frontend_assets_url('css/colors/theme-skin-color-set-1.css'); ?>" rel="stylesheet" type="text/css">
+<link href="public/frontend/css/colors/theme-skin-color-set-1.css" rel="stylesheet" type="text/css">
 
 <!-- external javascripts -->
-<script src="<?php echo frontend_assets_url('js/jquery-2.2.4.min.js'); ?>"></script>
-<script src="<?php echo frontend_assets_url('js/jquery-ui.min.js'); ?>"></script>
-<script src="<?php echo frontend_assets_url('js/bootstrap.min.js'); ?>"></script>
+<script src="public/frontend/js/jquery-2.2.4.min.js"></script>
+<script src="public/frontend/js/jquery-ui.min.js"></script>
+<script src="public/frontend/js/bootstrap.min.js"></script>
 <!-- JS | jquery plugin collection for this theme -->
-<script src="<?php echo frontend_assets_url('js/jquery-plugin-collection.js'); ?>"></script>
+<script src="public/frontend/js/jquery-plugin-collection.js"></script>
 
-<!-- Revolution Slider 5.x SCRIPTS -->
-<script src="<?php echo frontend_assets_url('js/revolution-slider/js/jquery.themepunch.tools.min.js'); ?>"></script>
-<script src="<?php echo frontend_assets_url('js/revolution-slider/js/jquery.themepunch.revolution.min.js'); ?>"></script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
 <body class="">
 <div id="wrapper">
   <!-- preloader -->
   <div id="preloader">
     <div id="spinner">
-      <img src="images/preloaders/4.gif" alt="">
+      <img src="public/frontend/images/preloaders/4.gif" alt="">
     </div>
     <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
   </div>
@@ -102,7 +102,7 @@
 
 <!-- Footer Scripts -->
 <!-- JS | Custom script for all pages -->
-<script src="<?php echo frontend_assets_url('js/custom.js'); ?>"></script>
+<script src="public/frontend/js/custom.js"></script>
 
 </body>
 </html>
