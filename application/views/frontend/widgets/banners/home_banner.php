4e96d3db79d61fc832538e8e595994302862123e
Varun Victor <varun.victor@fuego.qa>
2022-03-22 11:13:02 +0300

    Home Slider Frontend

--- application/views/frontend/widgets/banners/home_banner.php
+++ application/views/frontend/widgets/banners/home_banner.php
@@ -4,16 +4,24 @@
 <div class="rev_slider_wrapper">
   <div class="rev_slider" data-version="5.0">
     <ul>
-
+      <?php 
+      $i=0; 
+      foreach($sliders as $slider): 
+        $i++; 
+        $sliderImage = frontend_assets_url('images/slide/c1.jpg');
+        if($slide['image']!=''){
+          $sliderImage = frontend_uploads_url('sliders/images/'.$slide['image']);
+        }
+      ?>
       <!-- SLIDE 1 -->
-      <li data-index="rs-1" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-easein="default" data-easeout="default" data-masterspeed="default" data-thumb="" data-rotate="0" data-saveperformance="off" data-title="Slide 1" data-description="">
+      <li data-index="rs-<?php echo $i; ?>" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-easein="default" data-easeout="default" data-masterspeed="default" data-thumb="" data-rotate="0" data-saveperformance="off" data-title="<?php echo $slide['title']; ?>" data-description="">
            <!-- MAIN IMAGE -->
-           <img src="<?php echo frontend_assets_url('images/slide/c1.jpg'); ?>"  alt=""  data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-bgparallax="10" data-no-retina>
+           <img src="<?php echo $sliderImage; ?>"  alt="<?php echo $slide['title']; ?>"  data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-bgparallax="10" data-no-retina>
            <!-- LAYERS -->
 
            <!-- LAYER NR. 1 -->
            <div class="tp-caption tp-resizeme text-uppercase  bg-theme-colored-transparent text-white font-raleway border-left-theme-color-2-6px border-right-theme-color-2-6px pl-30 pr-30"
-              id="rs-2-layer-1"
+              id="rs-<?php echo $i; ?>-layer-1"
 
               data-x="['center']"
               data-hoffset="['0']"
@@ -33,12 +41,12 @@
               data-splitin="none"
               data-splitout="none"
               data-responsive_offset="on"
-              style="z-index: 7; white-space: nowrap; font-weight:400; border-radius: 30px;">Welcome to the NBSCHA
+              style="z-index: 7; white-space: nowrap; font-weight:400; border-radius: 30px;"><?php echo $slide['subtitle']; ?>
            </div>
 
            <!-- LAYER NR. 2 -->
            <div class="tp-caption tp-resizeme text-uppercase bg-theme-colored-transparent text-white font-raleway pl-30 pr-30"
-              id="rs-2-layer-2"
+              id="rs-<?php echo $i; ?>-layer-2"
 
               data-x="['center']"
               data-hoffset="['0']"
@@ -58,12 +66,12 @@
               data-splitin="none"
               data-splitout="none"
               data-responsive_offset="on"
-              style="z-index: 7; white-space: nowrap; font-weight:700; border-radius: 30px;">When you are not able. let us be of assistance
+              style="z-index: 7; white-space: nowrap; font-weight:700; border-radius: 30px;"><?php echo $slide['title']; ?>
            </div>
 
            <!-- LAYER NR. 3 -->
            <div class="tp-caption tp-resizeme text-white text-center bc-op"
-              id="rs-2-layer-3"
+              id="rs-<?php echo $i; ?>-layer-3"
 
               data-x="['center']"
               data-hoffset="['0']"
@@ -83,180 +91,10 @@
               data-splitin="none"
               data-splitout="none"
               data-responsive_offset="on"
-              style="z-index: 5; white-space: nowrap; letter-spacing:0px; font-weight:600;"><b>Providing the right services for special care to welcome<br>the residents to their new homes.</b>
+              style="z-index: 5; white-space: nowrap; letter-spacing:0px; font-weight:600;"><?php echo $slide['body']; ?>
            </div>
       </li>
-
-      <!-- SLIDE 2 -->
-      <li data-index="rs-2" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-easein="default" data-easeout="default" data-masterspeed="default" data-thumb="" data-rotate="0" data-saveperformance="off" data-title="Slide 2" data-description="">
-           <!-- MAIN IMAGE -->
-           <img src="<?php echo frontend_assets_url('images/slide/c2.jpg'); ?>"  alt=""  data-bgposition="center 40%" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-bgparallax="10" data-no-retina>
-           <!-- LAYERS -->
-
-           <!-- LAYER NR. 1 -->
-           <div class="tp-caption tp-resizeme text-uppercase  bg-theme-colored-transparent text-white font-raleway border-left-theme-color-2-6px border-right-theme-color-2-6px pl-30 pr-30"
-              id="rs-2-layer-1"
-
-              data-x="['center']"
-              data-hoffset="['0']"
-              data-y="['middle']"
-              data-voffset="['-90']"
-              data-fontsize="['28']"
-              data-lineheight="['54']"
-              data-width="none"
-              data-height="none"
-              data-whitespace="nowrap"
-              data-transform_idle="o:1;s:500"
-              data-transform_in="y:100;scaleX:1;scaleY:1;opacity:0;"
-              data-transform_out="x:left(R);s:1000;e:Power3.easeIn;s:1000;e:Power3.easeIn;"
-              data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-              data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-              data-start="1000"
-              data-splitin="none"
-              data-splitout="none"
-              data-responsive_offset="on"
-              style="z-index: 7; white-space: nowrap; font-weight:400; border-radius: 30px;">Join Our Network
-           </div>
-
-           <!-- LAYER NR. 2 -->
-           <div class="tp-caption tp-resizeme text-uppercase bg-theme-colored-transparent text-white font-raleway pl-30 pr-30"
-              id="rs-2-layer-2"
-
-              data-x="['center']"
-              data-hoffset="['0']"
-              data-y="['middle']"
-              data-voffset="['-20']"
-              data-fontsize="['48']"
-              data-lineheight="['70']"
-              data-width="none"
-              data-height="none"
-              data-whitespace="nowrap"
-              data-transform_idle="o:1;s:500"
-              data-transform_in="y:100;scaleX:1;scaleY:1;opacity:0;"
-              data-transform_out="x:left(R);s:1000;e:Power3.easeIn;s:1000;e:Power3.easeIn;"
-              data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-              data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-              data-start="1000"
-              data-splitin="none"
-              data-splitout="none"
-              data-responsive_offset="on"
-              style="z-index: 7; white-space: nowrap; font-weight:700; border-radius: 30px;">Become a Member
-           </div>
-
-           <!-- LAYER NR. 3 -->
-           <div class="tp-caption tp-resizeme text-white text-center bc-op"
-              id="rs-2-layer-3"
-
-              data-x="['center']"
-              data-hoffset="['0']"
-              data-y="['middle']"
-              data-voffset="['50']"
-              data-fontsize="['16']"
-              data-lineheight="['28']"
-              data-width="none"
-              data-height="none"
-              data-whitespace="nowrap"
-              data-transform_idle="o:1;s:500"
-              data-transform_in="y:100;scaleX:1;scaleY:1;opacity:0;"
-              data-transform_out="x:left(R);s:1000;e:Power3.easeIn;s:1000;e:Power3.easeIn;"
-              data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-              data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-              data-start="1400"
-              data-splitin="none"
-              data-splitout="none"
-              data-responsive_offset="on"
-              style="z-index: 5; white-space: nowrap; letter-spacing:0px; font-weight:600;"><b>Get listed on our website and access membership<br> benefits by registering your home.</b>
-           </div>
-
-
-
-      </li>
-
-      <!-- SLIDE 3 -->
-      <li data-index="rs-3" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-easein="default" data-easeout="default" data-masterspeed="default" data-thumb="" data-rotate="0" data-saveperformance="off" data-title="Slide 3" data-description="">
-           <!-- MAIN IMAGE -->
-            <img src="<?php echo frontend_assets_url('images/slide/c3.jpg'); ?>"  alt=""  data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-bgparallax="10" data-no-retina>
-            <!-- LAYERS -->
-
-            <!-- LAYER NR. 1 -->
-           <div class="tp-caption tp-resizeme text-uppercase  bg-theme-colored-transparent text-white font-raleway border-left-theme-color-2-6px border-right-theme-color-2-6px pl-30 pr-30"
-              id="rs-2-layer-1"
-
-              data-x="['center']"
-              data-hoffset="['0']"
-              data-y="['middle']"
-              data-voffset="['-90']"
-              data-fontsize="['28']"
-              data-lineheight="['54']"
-              data-width="none"
-              data-height="none"
-              data-whitespace="nowrap"
-              data-transform_idle="o:1;s:500"
-              data-transform_in="y:100;scaleX:1;scaleY:1;opacity:0;"
-              data-transform_out="x:left(R);s:1000;e:Power3.easeIn;s:1000;e:Power3.easeIn;"
-              data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-              data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-              data-start="1000"
-              data-splitin="none"
-              data-splitout="none"
-              data-responsive_offset="on"
-              style="z-index: 7; white-space: nowrap; font-weight:400; border-radius: 30px;">New Brunswick Special Care Homes
-           </div>
-
-           <!-- LAYER NR. 2 -->
-           <div class="tp-caption tp-resizeme text-uppercase bg-theme-colored-transparent text-white font-raleway pl-30 pr-30"
-              id="rs-2-layer-2"
-
-              data-x="['center']"
-              data-hoffset="['0']"
-              data-y="['middle']"
-              data-voffset="['-20']"
-              data-fontsize="['48']"
-              data-lineheight="['70']"
-              data-width="none"
-              data-height="none"
-              data-whitespace="nowrap"
-              data-transform_idle="o:1;s:500"
-              data-transform_in="y:100;scaleX:1;scaleY:1;opacity:0;"
-              data-transform_out="x:left(R);s:1000;e:Power3.easeIn;s:1000;e:Power3.easeIn;"
-              data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-              data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-              data-start="1000"
-              data-splitin="none"
-              data-splitout="none"
-              data-responsive_offset="on"
-              style="z-index: 7; white-space: nowrap; font-weight:700; border-radius: 30px;"> Browse our homes
-           </div>
-
-            <!-- LAYER NR. 3 -->
-           <div class="tp-caption tp-resizeme text-white text-center bc-op"
-                  id="rs-2-layer-3"
-
-              data-x="['center']"
-              data-hoffset="['0']"
-              data-y="['middle']"
-              data-voffset="['50']"
-              data-fontsize="['16']"
-              data-lineheight="['28']"
-              data-width="none"
-              data-height="none"
-              data-whitespace="nowrap"
-              data-transform_idle="o:1;s:500"
-              data-transform_in="y:100;scaleX:1;scaleY:1;opacity:0;"
-              data-transform_out="x:left(R);s:1000;e:Power3.easeIn;s:1000;e:Power3.easeIn;"
-              data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-              data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-              data-start="1400"
-              data-splitin="none"
-              data-splitout="none"
-              data-responsive_offset="on"
-              style="z-index: 5; white-space: nowrap; letter-spacing:0px; font-weight:600;">If you are interested in reserving a home,<br> please contact the owner directly by phone or email.
-           </div>
-
-
-           </div>
-       </li>
-
+      <?php endforeach; ?>
     </ul>
   </div>
   <!-- end .rev_slider -->
@@ -304,7 +142,7 @@
           }
         },
         bullets: {
-          enable:true,
+          enable:false,
           hide_onmobile:true,
           hide_under:600,
           style:"metis",
@@ -316,8 +154,7 @@
           v_align:"bottom",
           h_offset:0,
           v_offset:30,
-          space:5,
-          tmp:'<span class="tp-bullet-img-wrap">  <span class="tp-bullet-image"></span></span><span class="tp-bullet-title">{{title}}</span>'
+          space:5
         }
       },
       responsiveLevels: [1240, 1024, 778],
@@ -356,51 +193,4 @@
   });
 </script>
 <!-- Slider Revolution Ends -->
-
 </section>
-<?php /*
-<div class="home-slides owl-carousel owl-theme">
-    <?php 
-    foreach($sliders as $slider): 
-        $image = '';
-        if($slider['image']!=''){
-            $image = frontend_uploads_url('sliders/images/'.$slider['image']);
-        }
-    ?>
-        
-    <div class="main-banner <?php if($image==''){ echo "item-bg3"; } ?>"<?php if($image!=''){ ?> style="background-image:url('<?php echo $image; ?>');"<?php } ?>>
-        <div class="d-table">
-            <div class="d-table-cell">
-                <div class="container">
-                <div class="main-banner-content banner-content-center">
-                        <?php echo $slider['body']; ?>
-                        <div class="button-box">
-                            <?php if($slider['link_url']!='' && $slider['link_title']!=''){ ?>
-                            <a href="<?php echo $slider['link_url']; ?>" class="btn btn-primary"><?php echo $slider['link_title']; ?></a>
-                            <?php } ?>
-                            <?php 
-                            $videoUrl = '';
-                            if($slider['video']!=''){
-                                $videoUrl = $slider['video'];
-                            } else {
-                                if($slider['youtube_video']!=''){
-                                    $videoUrl = $slider['youtube_video'];
-                                }
-                            }
-                            if($videoUrl!=''){
-                            ?>
-                            <a href="<?php echo $videoUrl; ?>" class="video-btn popup-youtube"><i class="icofont-ui-play"></i> Watch Pormo Video</a>
-                            <?php } ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="shape1"><img src="<?php echo frontend_assets_url('img/shapes/1.png'); ?>" alt="shape1"></div>
-        <div class="shape2 rotateme"><img src="<?php echo frontend_assets_url('img/shapes/2.png'); ?>" alt="shape2"></div>
-        <div class="shape3 rotateme"><img src="<?php echo frontend_assets_url('img/shapes/3.png'); ?>" alt="shape3"></div>
-        <div class="shape4"><img src="<?php echo frontend_assets_url('img/shapes/4.png" alt="shape4'); ?>"></div>
-    </div>
-    <?php endforeach; ?>
-</div>
-*/ ?>
--- application/views/frontend/widgets/banners/inside_banner.php
+++ application/views/frontend/widgets/banners/inside_banner.php
@@ -1,4 +1,4 @@
-<section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="">
+<section class="inner-header divider parallax layer-overlay overlay-dark-5" data-bg-img="<?php echo $banner; ?>">
     <div class="container pt-70 pb-20">
         <!-- Section Content -->
         <div class="section-content">
@@ -6,7 +6,7 @@
             <div class="col-md-12">
                 <h2 class="title text-white text-center"><?php echo $title; ?></h2>
                 <ol class="breadcrumb text-left text-black mt-10">
-                <li><a href="<?php echo site_url('/'); ?>">Home</a></li>
+                <li class="text-gray-silver"><a href="<?php echo site_url('/'); ?>">Home</a></li>
                 <li class="active text-gray-silver"><?php echo $title; ?></li>
                 </ol>
             </div>
@@ -14,16 +14,3 @@
         </div>
     </div>
 </section>
-<?php
-/*<div class="page-title-area item-bg1" <?php if($banner!=''){ ?>style="background-image:url('<?php echo $banner; ?>');"<?php } ?>>
-    <div class="container">
-        <h1><?php echo $title; ?></h1>
-        <span><?php echo $subtitle; ?></span>
-        <ul>
-            <li><a href="<?php echo site_url('/'); ?>">Home</a></li>
-            <li><?php echo $title; ?></li>
-        </ul>
-    </div>
-</div>
-*/
-?>
