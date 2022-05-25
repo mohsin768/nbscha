63bfd6ee1cd3201c5df383ece5f263739c827731
Varun Victor <varun.victor@fuego.qa>
2022-05-25 19:20:11 +0530

    Working Hours

--- application/views/frontend/includes/header.php
+++ application/views/frontend/includes/header.php
@@ -7,7 +7,7 @@
                 <div class="col-md-7">
                 <div class="widget no-border m-0">
                     <ul class="list-inline sm-text-center mt-5">
-                      <li><?php echo $this->settings['WORKING_HOURS']; ?></li>
+                      <li><i class="fa fa-clock-o text-theme-colored"></i> <?php echo $this->settings['WORKING_HOURS']; ?></li>
                     </ul>
                 </div>
                 </div>
