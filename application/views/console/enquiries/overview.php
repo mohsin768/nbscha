9731bbf363f0ef3a8ad80510ccd27d8b610d2014
Varun Victor <varun.victor@fuego.qa>
2023-01-24 20:55:35 +0530

    Inquiries

--- application/views/console/enquiries/overview.php
+++ application/views/console/enquiries/overview.php
@@ -10,7 +10,7 @@ if($this->uri->segment(4)==""){
     <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
             <div class="x_title">
-                <h2>Enquiries</h2>
+                <h2>Inquiries</h2>
 
                 <div class="clearfix"></div>
             </div>
